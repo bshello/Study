084f85a91cab59e3fa270b8f56c3989899dadf6b
HyunseokCheong <jhs710000@gmail.com>
2022-08-02 10:20:47 +0900

    61~75

---
+++ 02_Algorithm/CHS/Codeup/6068.py
@@ -0,0 +1,9 @@
+N = int(input())
+if N >= 90:
+    print('A')
+elif N >= 70:
+    print('B')
+elif N >= 40:
+    print('C')
+else:
+    print('D')
---
+++ 02_Algorithm/CHS/Codeup/6069.py
@@ -0,0 +1,11 @@
+C = input()
+if C == 'A':
+    print('best!!!')
+elif C == 'B':
+    print('good!!')
+elif C == 'C':
+    print('run!')
+elif C == 'D':
+    print('slowly~')
+else:
+    print('what?')
