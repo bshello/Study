a5c2a2686426b9187ffd26b789c69172cfce0158
hwanny7 <asdryzx2@gmail.com>
2022-08-05 00:18:48 +0900

    final

--- 02_Algorithm/YSH/codeup/6098.py
+++ 02_Algorithm/YSH/codeup/6098.py
@@ -8,28 +8,22 @@ lst[1][1] = 9
 x, y = 1, 1
 n = 0
 while n != 1:
-    try:
-        if lst[x][y+1] == 0:
-            lst[x][y+1] = 9
-            y += 1
-        elif lst[x][y+1] == 2:
-            lst[x][y+1] = 9
-            n = 1
-        else:
-            try:
-                if lst[x+1][y] == 0:
-                    lst[x+1][y] = 9
-                    x += 1
-                elif lst[x+1][y] == 1:
-                    break
-                else:
-                    lst[x+1][y] = 9
-                    n = 1
-            except:
-                print("out of range")    # 8,8 을 break으로 둬야 했던 것 같은데.. 실패하는 케이스도 없는 것 같다
+    if lst[x][y+1] == 0:
+        lst[x][y+1] = 9
+        y += 1
+    elif lst[x][y+1] == 2:
+        lst[x][y+1] = 9
+        n = 1
 
-    except:
-        print("out of range")
+    else:
+        if lst[x+1][y] == 0:
+            lst[x+1][y] = 9
+            x += 1
+        elif lst[x+1][y] == 2:
+            lst[x+1][y] = 9
+            n = 1
+        elif x == 8 and y == 8:
+            n = 1
     
 for i in lst:
     print(*i)
