741460e73fc3295d6f7114ed41113d7129de4da3
bshello25 <bshello25@gmail.com>
2022-08-04 15:43:32 +0900

    97

---
+++ 02_Algorithm/Week01/KWW/Codeup/6097_27408598(AC).py
@@ -0,0 +1,34 @@
+﻿h, w = map(int, input().split())
+n = int(input())
+temp = []
+l = []
+d = []
+x = []
+y = []
+pan = []
+
+for a in range(n):
+    temp.append(list(map(int, input().split())))
+
+for b in range(len(temp)):
+    l.append(temp[b][0])
+    d.append(temp[b][1])
+    x.append(temp[b][2])
+    y.append(temp[b][3])
+
+for i in range(h):
+    pan.append([])
+    for j in range(w):
+        pan[i].append(0)
+
+for idx in range(len(l)):
+    for k in range(l[idx]):
+        if d[idx] == 0:
+            pan[x[idx]-1][y[idx]+k-1] = 1
+        if d[idx] == 1:
+            pan[x[idx]+k-1][y[idx]-1] = 1
+
+for row in range(len(pan)):
+    for col in range(len(pan[row])):
+        print(pan[row][col], end=" ")
+    print("")
